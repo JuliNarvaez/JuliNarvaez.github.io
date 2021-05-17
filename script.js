00e700ca2dd454ac1a879b27ca292d6e6476f934
JuliNarvaez <0720jano@gmail.com>
2021-05-17 13:10:33 -0500

    Darkmode button localstorage

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const btn = document.querySelector(".toggle");
+const darkTheme = 'dark-theme'; 
+let isDarkModeActived = JSON.parse(localStorage.getItem(darkTheme));
+
+function setDarkMode(isChecked){
+  document.body.classList.toggle(darkTheme, isChecked);
+  btn.checked = isChecked;
+}
+
+setDarkMode(isDarkModeActived);
+
 btn.addEventListener("click", function () {
-  document.body.classList.toggle("dark-theme");
+  localStorage.setItem(darkTheme, !isDarkModeActived);
+  setDarkMode(!isDarkModeActived);
+  isDarkModeActived = !isDarkModeActived;
 });
